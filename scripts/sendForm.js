519762a3e5faa8dfb6801225adc936654097db44
Ronaldmera <ronaldalme1230@gmail.com>
2025-06-02 10:53:45 -0500

    formulario de contacto funcional v1

---
+++ scripts/sendForm.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("LL7zHTKhOzljSR30E");
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      if (!this.checkValidity()) {
+        return;
+      }
+
+      let params = {
+        to_name: "Ronald",
+        from_name: document.getElementById("from_name").value,
+        from_email: document.getElementById("from_email").value,
+        comment: document.getElementById("comment").value,
+      };
+
+      emailjs
+        .send("service_wf0k9lp", "template_q6img3o", params)
+        .then((response) => {
+          console.log("Correo enviado con éxito:", response);
+          alert("Mensaje enviado correctamente");
+          this.reset();
+        })
+        .catch((error) => {
+          console.error("Error al enviar el correo:", error);
+          alert("Hubo un error al enviar el mensaje");
+        });
+    });
+});
