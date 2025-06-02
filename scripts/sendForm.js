b11cee2710ad253f1ba8f666268e813c726c79b4
Ronaldmera <ronaldalme1230@gmail.com>
2025-06-02 11:14:46 -0500

    loader agregado

--- scripts/sendForm.js
+++ scripts/sendForm.js
@@ -5,9 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     .getElementById("contact-form")
     .addEventListener("submit", function (event) {
       event.preventDefault();
+
+      const loader = document.querySelector(".box-loader");
       if (!this.checkValidity()) {
         return;
       }
+      loader.style.display = "flex"; // Mostrar loader
 
       let params = {
         to_name: "Ronald",
@@ -18,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       emailjs
         .send("service_wf0k9lp", "template_q6img3o", params)
+
         .then((response) => {
           console.log("Correo enviado con éxito:", response);
           alert("Mensaje enviado correctamente");
@@ -26,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch((error) => {
           console.error("Error al enviar el correo:", error);
           alert("Hubo un error al enviar el mensaje");
+        })
+        .finally(() => {
+          loader.style.display = "none"; // Ocultar loader después de la respuesta
         });
     });
 });
