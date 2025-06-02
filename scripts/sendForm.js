b6b9b91f527d5a132888e2d3f1afa573aedfd9fd
Ronaldmera <ronaldalme1230@gmail.com>
2025-06-02 11:59:37 -0500

    modal agregado en form

--- scripts/sendForm.js
+++ scripts/sendForm.js
@@ -7,9 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
       event.preventDefault();
 
       const loader = document.querySelector(".box-loader");
+      const modal = document.querySelector(".modal");
+      const textModal = document.querySelector(".text-status-form");
       if (!this.checkValidity()) {
         return;
       }
+      document.body.classList.add("no-scroll");
       loader.style.display = "flex"; // Mostrar loader
 
       let params = {
@@ -23,16 +26,22 @@ document.addEventListener("DOMContentLoaded", function () {
         .send("service_wf0k9lp", "template_q6img3o", params)
 
         .then((response) => {
-          console.log("Correo enviado con éxito:", response);
-          alert("Mensaje enviado correctamente");
+          //   console.log("Correo enviado con éxito:", response);
+          textModal.textContent = "Mensaje enviado correctamente";
+          const bsModal = new bootstrap.Modal(
+            document.getElementById("statusModal")
+          );
+          bsModal.show();
+
           this.reset();
         })
         .catch((error) => {
-          console.error("Error al enviar el correo:", error);
-          alert("Hubo un error al enviar el mensaje");
+          //   console.error("Error al enviar el correo:", error);
+          textModal.textContent = "Hubo un error al enviar el mensaje";
         })
         .finally(() => {
           loader.style.display = "none"; // Ocultar loader después de la respuesta
+          document.body.classList.remove("no-scroll"); //  Habilitar scroll otra vez
         });
     });
 });
