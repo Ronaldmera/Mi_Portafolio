95011bd6d90ca55217a5846b3a90807b56cd795b
Ronaldmera <ronaldalme1230@gmail.com>
2025-12-21 11:41:01 -0500

    iconos agregados y animacion de escritura en seccion Home

---
+++ scripts/writeText.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".writeText");
+
+  const spanText = "Desarrollador";
+  const normalText = " Junior";
+
+  container.innerHTML = "";
+
+  const span = document.createElement("span");
+  container.appendChild(span);
+
+  let i = 0;
+  let j = 0;
+
+  function escribirSpan() {
+    if (i < spanText.length) {
+      span.textContent += spanText.charAt(i);
+      i++;
+      setTimeout(escribirSpan, 100);
+    } else {
+      escribirTexto();
+    }
+  }
+
+  function escribirTexto() {
+    if (j < normalText.length) {
+      container.append(normalText.charAt(j));
+      j++;
+      setTimeout(escribirTexto, 100);
+    }
+  }
+
+  escribirSpan();
+});
