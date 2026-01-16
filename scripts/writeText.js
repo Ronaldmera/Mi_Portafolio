8e93b2249c265f9f919bbcedb79a63ce2b951d1e
Ronaldmera <ronaldalme1230@gmail.com>
2026-01-16 11:19:23 -0500

    barra progreso agregada

--- scripts/writeText.js
+++ scripts/writeText.js
@@ -34,3 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const track = document.querySelector(".marquee-track");
+
+// codigo de la barra de porcentaje
+
+const barra = document.querySelector(".barraProgreso");
+window.addEventListener("scroll", () => {
+  const altura = document.documentElement.scrollHeight - window.innerHeight;
+  const progreso = (window.scrollY / altura) * 100;
+  barra.style.width = progreso + "%";
+});
