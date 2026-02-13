90580bb23f776e952c1cc481a57d5374f10f5b3d
Ronaldmera <ronaldalme1230@gmail.com>
2026-02-13 13:26:38 -0500

    animaciones fade-up eliminadas

--- scripts/dateFooter.js
+++ scripts/dateFooter.js
@@ -1,22 +1,3 @@
 const dateContainer = document.getElementById("date");
 let today = new Date();
 dateContainer.textContent = today.getFullYear();
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-        observer.unobserve(entry.target);
-      }
-    });
-  },
-  {
-    threshold: 0.2,
-    rootMargin: "0px 0px -20% 0px",
-  },
-);
-
-document.querySelectorAll(".fade-up").forEach((el) => {
-  observer.observe(el);
-});
