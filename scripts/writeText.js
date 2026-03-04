a051bf2411f0ab30498d95443c157510bac529b8
Ronaldmera <ronaldalme1230@gmail.com>
2026-03-04 18:56:45 -0500

    cambio de tipografia y optimizacion en general

--- scripts/writeText.js
+++ scripts/writeText.js
@@ -1,38 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".writeText");
-
-  const spanText = "Transformo";
-  const normalText = " ideas en soluciones de software";
-
-  container.innerHTML = "";
-
-  const span = document.createElement("span");
-  container.appendChild(span);
-
-  let i = 0;
-  let j = 0;
-
-  function escribirSpan() {
-    if (i < spanText.length) {
-      span.textContent += spanText.charAt(i);
-      i++;
-      setTimeout(escribirSpan, 100);
-    } else {
-      escribirTexto();
-    }
-  }
-
-  function escribirTexto() {
-    if (j < normalText.length) {
-      container.append(normalText.charAt(j));
-      j++;
-      setTimeout(escribirTexto, 100);
-    }
-  }
-
-  escribirSpan();
-});
-
 const track = document.querySelector(".marquee-track");
 
 // codigo de la barra de porcentaje
