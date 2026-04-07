f873c7deddace698145b6e2f4eda286a5b35ff97
Ronaldmera <ronaldalme1230@gmail.com>
2026-04-07 12:06:25 -0500

    nueva imagen en card adm Tasks agregada

---
+++ scripts/createCardProject.js
@@ -0,0 +1,77 @@
+const track = document.querySelector(".marquee-track");
+
+// codigo de la barra de porcentaje
+
+const barra = document.querySelector(".barraProgreso");
+window.addEventListener("scroll", () => {
+  const altura = document.documentElement.scrollHeight - window.innerHeight;
+  const progreso = (window.scrollY / altura) * 100;
+  barra.style.width = progreso + "%";
+});
+
+// codigo mostrar y ocultar  cards en projectos
+let btn = document.querySelector(".btn-show-more");
+let containerProjects = document.querySelector(".container-projects");
+let card = `
+  <div class="col-md-6 col-lg-4 d-flex card-extra">
+    <div class="card h-100 w-100">
+      <img
+        src="/assets/images/projects/wellWoman.webp"
+        class="card-img-top"
+        alt="Well Woman"
+        loading="lazy"
+        draggable="false"
+      />
+      <div class="card-body d-flex flex-column">
+        <h4 class="card-title text-center">Well Woman</h4>
+        <p class="card-text">
+          Plataforma web de bienestar femenino con consejos, foros y
+          tienda especializada.
+        </p>
+        <div class="technologies pb-3">
+          <h5 class="text-center">Tecnologías</h5>
+          <div class="container-tech-list">
+            <ul
+              class="list-unstyled d-flex gap-2 flex-wrap justify-content-center"
+            >
+              <li class="p-1 rounded-1">HTML</li>
+              <li class="p-1 rounded-1">CSS</li>
+              <li class="p-1 rounded-1">JavaScript</li>
+              <li class="p-1 rounded-1">MySQL</li>
+              <li class="p-1 rounded-1">GitHub</li>
+              <li class="p-1 rounded-1">Laravel</li>
+            </ul>
+          </div>
+        </div>
+
+        <a
+          href="https://github.com/HaJuegos/WellWoman"
+          target="_blank"
+          class="btn mt-auto d-flex justify-content-center"
+        >
+          Explorar
+        </a>
+      </div>
+    </div>
+  </div>
+`;
+
+function agregarTextoBtn(btn) {
+  let text = btn.textContent.trim();
+  if (text == "Ver más") {
+    btn.textContent = "Ver menos";
+  } else {
+    btn.textContent = "Ver más";
+  }
+}
+let existscard = false;
+btn.addEventListener("click", () => {
+  if (!existscard) {
+    containerProjects.insertAdjacentHTML("beforeend", card);
+    existscard = true;
+  } else {
+    containerProjects.removeChild(document.querySelector(".card-extra"));
+    existscard = false;
+  }
+  agregarTextoBtn(btn);
+});
