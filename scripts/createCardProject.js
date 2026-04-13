e9af21b916e02e0126bb8cb2f2ddc4b27b0d8fe2
Ronaldmera <ronaldalme1230@gmail.com>
2026-04-13 11:16:25 -0500

    Aos libreria instalada

--- scripts/createCardProject.js
+++ scripts/createCardProject.js
@@ -2,12 +2,12 @@ const track = document.querySelector(".marquee-track");
 
 // codigo de la barra de porcentaje
 
-const barra = document.querySelector(".barraProgreso");
-window.addEventListener("scroll", () => {
-  const altura = document.documentElement.scrollHeight - window.innerHeight;
-  const progreso = (window.scrollY / altura) * 100;
-  barra.style.width = progreso + "%";
-});
+// const barra = document.querySelector(".barraProgreso");
+// window.addEventListener("scroll", () => {
+//   const altura = document.documentElement.scrollHeight - window.innerHeight;
+//   const progreso = (window.scrollY / altura) * 100;
+//   barra.style.width = progreso + "%";
+// });
 
 // codigo mostrar y ocultar  cards en projectos
 let btn = document.querySelector(".btn-show-more");
--- scripts/dateFooter.js
+++ scripts/dateFooter.js
@@ -1,3 +1,9 @@
 const dateContainer = document.getElementById("date");
 let today = new Date();
 dateContainer.textContent = today.getFullYear();
+AOS.init({
+  //   duration: 700,
+  once: true,
+  offset: 100,
+  disable: "mobile",
+});
