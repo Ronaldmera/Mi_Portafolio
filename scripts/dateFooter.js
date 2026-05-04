43d8432c0d600285be8e6104a691eacd4787cb98
Ronaldmera <ronaldalme1230@gmail.com>
2026-05-04 12:56:17 -0500

    swiper JS agregado en seccion proyectos

--- scripts/dateFooter.js
+++ scripts/dateFooter.js
@@ -2,8 +2,67 @@ const dateContainer = document.getElementById("date");
 let today = new Date();
 dateContainer.textContent = today.getFullYear();
 AOS.init({
-  //   duration: 700,
   once: true,
   offset: 100,
   disable: "mobile",
+});
+const swiperWeb = new Swiper(".swiper-web", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  grabCursor: true,
+  autoplay: {
+    delay: 5500, // tiempo entre cada movimiento
+    disableOnInteraction: false, // sigue después de tocar
+  },
+  // slidesPerView: "auto",
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    650: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+    },
+  },
+});
+const swiperAutomatizacion = new Swiper(".swiper-automatizacion", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  // slidesPerView: "auto",
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    650: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
 });
