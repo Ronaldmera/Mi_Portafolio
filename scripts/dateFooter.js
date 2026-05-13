97715c939bef2c90cbb8d28ca67c02ad36ec2a3a
Ronaldmera <ronaldalme1230@gmail.com>
2026-05-13 15:06:30 -0500

    botones prev y next agregados en slider de projects

--- scripts/dateFooter.js
+++ scripts/dateFooter.js
@@ -23,6 +23,11 @@ const swiperWeb = new Swiper(".swiper-web", {
   pagination: {
     el: ".swiper-pagination",
   },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   scrollbar: {
     el: ".swiper-scrollbar",
   },
